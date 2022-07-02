29220dc135473b21d555b23bfd162c4b3b697b47
XIII___ <cyjallet@gmail.com>
2022-07-02 21:39:19 +0200

    (+) /tp

--- src/main/java/com/xiii/libertycity/core/commands/Teleport.java
+++ src/main/java/com/xiii/libertycity/core/commands/Teleport.java
@@ -11,26 +11,36 @@ public class Teleport implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        Player player = (Player) sender;
+
         if(command.getName().equalsIgnoreCase("tp") || command.getName().equalsIgnoreCase("teleport")) {
             if(sender.hasPermission("LibertyCity.teleport")) {
-                Player target = Bukkit.getServer().getPlayer(args[0]);
-                Player target2 = Bukkit.getServer().getPlayer(args[1]);
                 if(args.length < 1) sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! /tp <Joueur> (Joueur)");
                 if(args.length == 1) {
-                    if(target.isOnline()) {
-                        Bukkit.getPlayer(String.valueOf(sender)).teleport(target);
-                        sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été téléporté a §6" + target.getName());
-                    } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cAttention! Ce joueur n'est pas en ligne");
+                    if(Bukkit.getServer().getPlayer(args[0]) != null) {
+                        Player target = Bukkit.getServer().getPlayer(args[0]);
+                        if (target.isOnline()) {
+                            player.teleport(target);
+                            sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été téléporté a §6" + target.getName());
+                        } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cAttention! Ce joueur n'est pas en ligne");
+                    }
                 } else if(args.length == 2) {
-                    if(target.isOnline() && target2.isOnline()) {
-                        Bukkit.getPlayer(String.valueOf(target)).teleport(target2);
-                        sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez téléporté §6" + target.getName() + " §fà §6" + target2.getName());
-                        target.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été téléporté à §6" + target2.getName());
-                    } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cAttention! Un des joueurs n'est pas en ligne");
+                    if(Bukkit.getServer().getPlayer(args[0]) != null) {
+                        Player target = Bukkit.getServer().getPlayer(args[0]);
+                        if (Bukkit.getServer().getPlayer(args[1]) != null) {
+                            Player target2 = Bukkit.getServer().getPlayer(args[1]);
+                            if (target.isOnline() && target2.isOnline()) {
+                                target.teleport(target2);
+                                sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez téléporté §6" + target.getName() + " §fà §6" + target2.getName());
+                                target.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été téléporté à §6" + target2.getName());
+                            } else
+                                sender.sendMessage("§2§lLiberty§a§lCity §7» §cAttention! Un des joueurs n'est pas en ligne");
+                        }
+                    }
                 } else if(args.length > 2) sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! Trop d'arguments");
             } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
         }
 
-        return false;
+        return true;
     }
 }
