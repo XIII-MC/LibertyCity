7a7322c83ceac88a961184ac816467455f0fbf31
XIII___ <cyjallet@gmail.com>
2022-07-02 21:35:46 +0200

    (~) /clear /ci

--- src/main/java/com/xiii/libertycity/core/commands/ClearInv.java
+++ src/main/java/com/xiii/libertycity/core/commands/ClearInv.java
@@ -11,16 +11,18 @@ public class ClearInv implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        Player player = (Player) sender;
+
         if(command.getName().equalsIgnoreCase("clear") || command.getName().equalsIgnoreCase("ci")) {
             if(sender.hasPermission("LibertyCity.clear")) {
                 if(args.length < 1) {
-                    Bukkit.getPlayer(String.valueOf(sender)).getInventory().clear();
+                    player.getInventory().clear();
                     sender.sendMessage("§2§lLiberty§a§lCity §7» §fVotre inventaire a été néttoyé");
                 } else if(args.length == 1) {
                     Player target = Bukkit.getServer().getPlayer(args[0]);
                     if(sender.hasPermission("LibertyCity.clear.others")) {
                         if(target.isOnline()) {
-                            Bukkit.getPlayer(String.valueOf(target)).getInventory().clear();
+                            target.getInventory().clear();
                             sender.sendMessage("§2§lLiberty§a§lCity §7» §fInventaire de §6" + target.getName() + " §fnéttoyé");
                             target.sendMessage("§2§lLiberty§a§lCity §7» §fVotre inventaire a été néttoyé");
                         } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cAttention! Ce joueur n'est pas en ligne");
@@ -29,6 +31,6 @@ public class ClearInv implements CommandExecutor {
             } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
         }
 
-        return false;
+        return true;
     }
 }
