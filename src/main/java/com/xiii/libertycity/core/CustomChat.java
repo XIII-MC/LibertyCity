65498b324865980b187a0c420d0986713f88d754
XIII___ <cyjallet@gmail.com>
2022-07-02 10:49:47 +0200

    (*) CooldownChat

--- src/main/java/com/xiii/libertycity/core/CustomChat.java
+++
@@ -1,6 +0,0 @@
-package com.xiii.libertycity.core;
-
-import org.bukkit.event.Listener;
-
-public class CustomChat implements Listener {
-}
--- src/main/java/com/xiii/libertycity/core/commands/CooldownChat.java
+++ src/main/java/com/xiii/libertycity/core/commands/CooldownChat.java
@@ -1,5 +1,7 @@
 package com.xiii.libertycity.core.commands;
 
+import com.xiii.libertycity.core.data.Data;
+import com.xiii.libertycity.core.data.ServerData;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,21 +13,21 @@ public class CooldownChat implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        /*if(command.getName().equalsIgnoreCase("cooldownchat") || command.getName().equalsIgnoreCase("cdchat")) {
+        if(command.getName().equalsIgnoreCase("cooldownchat") || command.getName().equalsIgnoreCase("cdchat")) {
             if(sender.hasPermission("LibertyCity.cooldownchat.use")) {
-                ServerData sData = com.xiii.libertycity.core.data.server.Data.sData.getServerData(Bukkit.getServer());
+                ServerData server = Data.data.getServerData(Bukkit.getServer());
 
                 if(args.length == 0) {
                     sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! Usage: /cooldownchat [TEMPS] (NB: Temps en MS)");
                 } else if(args.length >= 1) {
                     int newIntConverted = 0;
                     newIntConverted = Integer.parseInt(args[0]);
-                    assert sData != null;
-                    if(newIntConverted == sData.chatCooldownGlobal) {
-                        sender.sendMessage("§2§lLiberty§a§lCity §7» §cAttention! Le cooldown est déja a §6" + newIntConverted + "§ems");
+                    assert server != null;
+                    if(newIntConverted == server.chatCooldownGlobal) {
+                        sender.sendMessage("§2§lLiberty§a§lCity §7» §cAttention! Le cooldown est déja a §6" + newIntConverted + "§6ms");
                     } else {
-                        sData.chatCooldownGlobal = newIntConverted;
-                        sender.sendMessage("§2§lLiberty§a§lCity §7» §fLe cooldown est désormais à §6" + newIntConverted + "§ems");
+                        server.chatCooldownGlobal = newIntConverted;
+                        sender.sendMessage("§2§lLiberty§a§lCity §7» §fLe cooldown est désormais à §6" + newIntConverted + "§6ms");
                         for(Player p : Bukkit.getOnlinePlayers()) {
                             if(p.hasPermission("LibertyCity.cooldownchat.use")) p.sendMessage("§4§LSTAFF §7» §8" + sender.getName() + " §7à mis le cooldown pour le chat §8GLOBAL §7à §8" + newIntConverted + "§8ms");
                         }
@@ -33,8 +35,7 @@ public class CooldownChat implements CommandExecutor {
                 }
                 return true;
             }
-        }*/
-
-        return false;
+        }
+    return false;
     }
 }
