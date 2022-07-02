2b22677e03d3c1ac1228063975b8279f9f909f9b
XIII___ <cyjallet@gmail.com>
2022-07-02 10:54:46 +0200

    (*) Ping

--- src/main/java/com/xiii/libertycity/core/commands/Ping.java
+++ src/main/java/com/xiii/libertycity/core/commands/Ping.java
@@ -5,7 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import static com.xiii.libertycity.core.utils.PingUtility.getPing;
+import static com.xiii.libertycity.core.utils.PingUtil.getPing;
 
 public class Ping implements CommandExecutor {
 
