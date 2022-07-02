debb8d16ec262e23ded96f62713a7e515e0358e8
XIII___ <cyjallet@gmail.com>
2022-07-02 21:37:38 +0200

    (+) /gamemode /gm

--- src/main/java/com/xiii/libertycity/core/commands/Gamemode.java
+++ src/main/java/com/xiii/libertycity/core/commands/Gamemode.java
@@ -14,18 +14,20 @@ public class Gamemode implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        Player player = (Player) sender;
+
         if(command.getName().equalsIgnoreCase("gm") || command.getName().equalsIgnoreCase("gamemode")) {
             if(sender.hasPermission("LibertyCity.gamemode")) {
-                Player target = getServer().getPlayer(args[1]);
 
                 if(args[0].equalsIgnoreCase("s") || args[0].equalsIgnoreCase("survie") || args[0].equalsIgnoreCase("survival")) {
                     if(sender.hasPermission("LibertyCity.gamemode.survival")) {
                         if(args.length == 1 ) {
-                            Bukkit.getPlayer(String.valueOf(sender)).setGameMode(GameMode.SURVIVAL);
+                            player.setGameMode(GameMode.SURVIVAL);
                             sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§nsurvie");
                         } else if(args.length == 2) {
+                            Player target = getServer().getPlayer(args[1]);
                             if(target.isOnline()) {
-                                Bukkit.getPlayer(String.valueOf(target)).setGameMode(GameMode.SURVIVAL);
+                                target.setGameMode(GameMode.SURVIVAL);
                                 sender.sendMessage("§2§lLiberty§a§lCity §7» §6" + target.getName() + "êtes désormais en §e§nsurvie");
                                 target.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§nsurvie");
                             }
@@ -36,11 +38,12 @@ public class Gamemode implements CommandExecutor {
                 if(args[0].equalsIgnoreCase("c") || args[0].equalsIgnoreCase("creatif") || args[0].equalsIgnoreCase("creative")) {
                     if(sender.hasPermission("LibertyCity.gamemode.creative")) {
                         if(args.length == 1 ) {
-                            Bukkit.getPlayer(String.valueOf(sender)).setGameMode(GameMode.CREATIVE);
+                            player.setGameMode(GameMode.CREATIVE);
                             sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§ncréatif");
                         } else if(args.length == 2) {
+                            Player target = getServer().getPlayer(args[1]);
                             if(target.isOnline()) {
-                                Bukkit.getPlayer(String.valueOf(target)).setGameMode(GameMode.CREATIVE);
+                                target.setGameMode(GameMode.CREATIVE);
                                 sender.sendMessage("§2§lLiberty§a§lCity §7» §6" + target.getName() + "êtes désormais en §e§ncréatif");
                                 target.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§ncréatif");
                             }
@@ -51,11 +54,12 @@ public class Gamemode implements CommandExecutor {
                 if(args[0].equalsIgnoreCase("sp") || args[0].equalsIgnoreCase("spectateur") || args[0].equalsIgnoreCase("spectator")) {
                     if(sender.hasPermission("LibertyCity.gamemode.spectator")) {
                         if(args.length == 1 ) {
-                            Bukkit.getPlayer(String.valueOf(sender)).setGameMode(GameMode.SPECTATOR);
+                            player.setGameMode(GameMode.SPECTATOR);
                             sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§nspectateur");
                         } else if(args.length == 2) {
+                            Player target = getServer().getPlayer(args[1]);
                             if(target.isOnline()) {
-                                Bukkit.getPlayer(String.valueOf(target)).setGameMode(GameMode.SPECTATOR);
+                                target.setGameMode(GameMode.SPECTATOR);
                                 sender.sendMessage("§2§lLiberty§a§lCity §7» §6" + target.getName() + "êtes désormais en §e§nspectateur");
                                 target.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§nspectateur");
                             }
@@ -66,11 +70,12 @@ public class Gamemode implements CommandExecutor {
                 if(args[0].equalsIgnoreCase("a") || args[0].equalsIgnoreCase("aventure") || args[0].equalsIgnoreCase("adventure")) {
                     if(sender.hasPermission("LibertyCity.gamemode.adventure")) {
                         if(args.length == 1 ) {
-                            Bukkit.getPlayer(String.valueOf(sender)).setGameMode(GameMode.ADVENTURE);
+                            player.setGameMode(GameMode.ADVENTURE);
                             sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§naventure");
                         } else if(args.length == 2) {
+                            Player target = getServer().getPlayer(args[1]);
                             if(target.isOnline()) {
-                                Bukkit.getPlayer(String.valueOf(target)).setGameMode(GameMode.ADVENTURE);
+                                target.setGameMode(GameMode.ADVENTURE);
                                 sender.sendMessage("§2§lLiberty§a§lCity §7» §6" + target.getName() + "êtes désormais en §e§naventure");
                                 target.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§naventure");
                             }
@@ -80,7 +85,7 @@ public class Gamemode implements CommandExecutor {
             } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
         }
 
-        return false;
+        return true;
     }
 
     public static class GMC implements CommandExecutor {
@@ -90,22 +95,23 @@ public class Gamemode implements CommandExecutor {
 
             if (command.getName().equalsIgnoreCase("gmc")) {
                 if (sender.hasPermission("LibertyCity.gamemode.creative")) {
-                    Player target = getServer().getPlayer(args[1]);
                     if (sender.hasPermission("LibertyCity.gamemode.creative")) {
-                        if (args.length == 1) {
-                            Bukkit.getPlayer(String.valueOf(sender)).setGameMode(GameMode.CREATIVE);
+                        if (args.length < 1) {
+                            Player player = (Player) sender;
+                            player.setGameMode(GameMode.CREATIVE);
                             sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§ncréatif");
-                        } else if (args.length == 2) {
+                        } else if (args.length == 1) {
+                            Player target = Bukkit.getServer().getPlayer(args[0]);
                             if (target.isOnline()) {
-                                Bukkit.getPlayer(String.valueOf(target)).setGameMode(GameMode.CREATIVE);
+                                target.setGameMode(GameMode.CREATIVE);
                                 sender.sendMessage("§2§lLiberty§a§lCity §7» §6" + target.getName() + "êtes désormais en §e§ncréatif");
                                 target.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§ncréatif");
                             }
-                        }
+                        } else if(args.length > 1) sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! Trop d'arguments");
                     } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
                 } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
             }
-            return false;
+            return true;
         }
     }
 
@@ -116,22 +122,23 @@ public class Gamemode implements CommandExecutor {
 
             if (command.getName().equalsIgnoreCase("gms")) {
                 if (sender.hasPermission("LibertyCity.gamemode.survival")) {
-                    Player target = getServer().getPlayer(args[1]);
                     if (sender.hasPermission("LibertyCity.gamemode.survival")) {
-                        if (args.length == 1) {
-                            Bukkit.getPlayer(String.valueOf(sender)).setGameMode(GameMode.SURVIVAL);
+                        if (args.length < 1) {
+                            Player player = (Player) sender;
+                            player.setGameMode(GameMode.SURVIVAL);
                             sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§nsurvie");
-                        } else if (args.length == 2) {
+                        } else if (args.length == 1) {
+                            Player target = getServer().getPlayer(args[0]);
                             if (target.isOnline()) {
-                                Bukkit.getPlayer(String.valueOf(target)).setGameMode(GameMode.SURVIVAL);
+                                target.setGameMode(GameMode.SURVIVAL);
                                 sender.sendMessage("§2§lLiberty§a§lCity §7» §6" + target.getName() + "êtes désormais en §e§nsurvie");
                                 target.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§nsurvie");
                             }
-                        }
+                        } else if(args.length > 1) sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! Trop d'arguments");
                     } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
                 } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
             }
-            return false;
+            return true;
         }
     }
 
@@ -142,22 +149,23 @@ public class Gamemode implements CommandExecutor {
 
             if (command.getName().equalsIgnoreCase("gma")) {
                 if (sender.hasPermission("LibertyCity.gamemode.adventure")) {
-                    Player target = getServer().getPlayer(args[1]);
                     if (sender.hasPermission("LibertyCity.gamemode.adventure")) {
-                        if (args.length == 1) {
-                            Bukkit.getPlayer(String.valueOf(sender)).setGameMode(GameMode.ADVENTURE);
+                        if (args.length < 1) {
+                            Player player = (Player) sender;
+                            player.setGameMode(GameMode.ADVENTURE);
                             sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§naventure");
-                        } else if (args.length == 2) {
+                        } else if (args.length == 1) {
+                            Player target = Bukkit.getServer().getPlayer(args[0]);
                             if (target.isOnline()) {
-                                Bukkit.getPlayer(String.valueOf(target)).setGameMode(GameMode.ADVENTURE);
+                                target.setGameMode(GameMode.ADVENTURE);
                                 sender.sendMessage("§2§lLiberty§a§lCity §7» §6" + target.getName() + "êtes désormais en §e§naventure");
                                 target.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§naventure");
                             }
-                        }
+                        } else if(args.length > 1) sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! Trop d'arguments");
                     } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
                 } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
             }
-            return false;
+            return true;
         }
     }
 
@@ -168,22 +176,23 @@ public class Gamemode implements CommandExecutor {
 
             if (command.getName().equalsIgnoreCase("gmsp")) {
                 if (sender.hasPermission("LibertyCity.gamemode.spectator")) {
-                    Player target = getServer().getPlayer(args[1]);
                     if (sender.hasPermission("LibertyCity.gamemode.spectator")) {
-                        if (args.length == 1) {
-                            Bukkit.getPlayer(String.valueOf(sender)).setGameMode(GameMode.SPECTATOR);
+                        if (args.length < 1) {
+                            Player player = (Player) sender;
+                            player.setGameMode(GameMode.SPECTATOR);
                             sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§nspectateur");
-                        } else if (args.length == 2) {
+                        } else if (args.length == 1) {
+                            Player target = Bukkit.getServer().getPlayer(args[0]);
                             if (target.isOnline()) {
-                                Bukkit.getPlayer(String.valueOf(target)).setGameMode(GameMode.SPECTATOR);
+                                target.setGameMode(GameMode.SPECTATOR);
                                 sender.sendMessage("§2§lLiberty§a§lCity §7» §6" + target.getName() + "êtes désormais en §e§nspectateur");
                                 target.sendMessage("§2§lLiberty§a§lCity §7» §fVous êtes désormais en §e§nspectateur");
                             }
-                        }
+                        } else if(args.length > 1) sender.sendMessage("§2§lLiberty§a§lCity §7» §cErreur! Trop d'arguments");
                     } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
                 } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante.");
             }
-            return false;
+            return true;
         }
     }
 }
