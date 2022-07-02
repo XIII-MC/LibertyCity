4838d16cebace17f8f142855680323d6d2990c92
XIII___ <cyjallet@gmail.com>
2022-07-02 21:36:38 +0200

    (+) /feed

--- src/main/java/com/xiii/libertycity/core/commands/Feed.java
+++ src/main/java/com/xiii/libertycity/core/commands/Feed.java
@@ -11,16 +11,18 @@ public class Feed implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        Player player = (Player) sender;
+
         if(command.getName().equalsIgnoreCase("feed")) {
             if(sender.hasPermission("LibertyCity.feed")) {
                 if(args.length < 1) {
-                    Bukkit.getPlayer(String.valueOf(sender)).setFoodLevel(20);
+                    player.setFoodLevel(20);
                     sender.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été rassasié");
                 } else if(args.length == 1) {
                     Player target = Bukkit.getServer().getPlayer(args[0]);
                     if(sender.hasPermission("LibertyCity.feed.others")) {
                         if (target.isOnline()) {
-                            Bukkit.getPlayer(String.valueOf(target)).setFoodLevel(20);
+                            target.setFoodLevel(20);
                             target.sendMessage("§2§lLiberty§a§lCity §7» §fVous avez été rassasié");
                             sender.sendMessage("§2§lLiberty§a§lCity §7» §6" + target.getName() + " §fa été rassasié");
                         } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cAttention! Ce joueur n'est pas en ligne");
@@ -29,6 +31,6 @@ public class Feed implements CommandExecutor {
             } else sender.sendMessage("§2§lLiberty§a§lCity §7» §cPermission Insuffisante");
         }
 
-        return false;
+        return true;
     }
 }
